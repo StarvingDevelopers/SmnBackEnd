dfc9865a6f8fb5949cca7fb29258b2ec596226ff
Felipe <feliperos.simao@gmail.com>
2024-06-18 19:17:35 -0300

    refactor(controller): removing old mail methods.

--- src/main/java/tech/starvingdevelopers/smnbackend/controllers/AccountController.java
+++ src/main/java/tech/starvingdevelopers/smnbackend/controllers/AccountController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import tech.starvingdevelopers.smnbackend.models.dto.account.CreateAccountDTO;
+import tech.starvingdevelopers.smnbackend.models.dto.account.UpdateAccountDTO;
 import tech.starvingdevelopers.smnbackend.models.entities.Account;
 import tech.starvingdevelopers.smnbackend.services.AccountService;
 
@@ -18,31 +19,25 @@ public class AccountController {
 
     @PostMapping("/create")
     public ResponseEntity<Account> createAccount(@RequestBody @Validated CreateAccountDTO createAccountDTO) {
-        Account account = accountService.createAccount(createAccountDTO);
+        Account account = this.accountService.createAccount(createAccountDTO);
         return ResponseEntity.ok(account);
     }
 
-    @GetMapping("/username/{username}")
+    @GetMapping("/{username}")
     public ResponseEntity<Account> getAccount(@PathVariable String username) {
         Account account = this.accountService.getAccountByUsername(username);
         return ResponseEntity.ok(account);
     }
 
-    @GetMapping("/email/{email}")
-    public ResponseEntity<Account> getAccountByEmail(@PathVariable String email) {
-        Account account = this.accountService.getAccountByEmail(email);
+    @PostMapping("/update/{username}")
+    public ResponseEntity<Account> updateAccount(@PathVariable String username, @RequestBody @Validated UpdateAccountDTO updateAccountDTO) {
+        Account account = this.accountService.updateAccountByUsername(username, updateAccountDTO);
         return ResponseEntity.ok(account);
     }
 
     @DeleteMapping("/delete/{username}")
-    public ResponseEntity<Boolean> deleteAccount(@PathVariable String username) {
-        boolean result = this.accountService.deleteAccountByUsername(username);
-        return ResponseEntity.ok(result);
-    }
-
-    @DeleteMapping("/delete/{email}")
-    public ResponseEntity<Boolean> deleteAccountByEmail(@PathVariable String email) {
-        boolean result = this.accountService.deleteAccountByEmail(email);
-        return ResponseEntity.ok(result);
+    public ResponseEntity<Void> deleteAccount(@PathVariable String username) {
+        this.accountService.deleteAccountByUsername(username);
+        return ResponseEntity.ok().build();
     }
 }
