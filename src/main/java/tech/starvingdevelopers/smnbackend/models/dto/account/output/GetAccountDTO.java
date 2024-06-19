9037fadaaf86dd1d774703aba29fd60567b7179d
Felipe <feliperos.simao@gmail.com>
2024-06-19 10:00:48 -0300

    refactor(dto): added params.

--- src/main/java/tech/starvingdevelopers/smnbackend/models/dto/account/output/GetAccountDTO.java
+++ src/main/java/tech/starvingdevelopers/smnbackend/models/dto/account/output/GetAccountDTO.java
@@ -2,5 +2,5 @@ package tech.starvingdevelopers.smnbackend.models.dto.account.output;
 
 import java.time.LocalDate;
 
-public record GetAccountDTO(String username, String nickname, String email, String gender, LocalDate birthdate) {
+public record GetAccountDTO(int id, String username, String nickname, String email, String gender, LocalDate birthdate, LocalDate createdAt) {
 }
