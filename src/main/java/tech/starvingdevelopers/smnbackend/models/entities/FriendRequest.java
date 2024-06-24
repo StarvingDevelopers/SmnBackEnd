2098bc75740a4b2c314bf2ed91a02f58817c7b44
FelipeRos19 <feliperos.simao@gmail.com>
2024-06-24 12:26:28 -0300

    feat(cache): implementing caching in friend route.

--- src/main/java/tech/starvingdevelopers/smnbackend/models/entities/FriendRequest.java
+++ src/main/java/tech/starvingdevelopers/smnbackend/models/entities/FriendRequest.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,7 +16,7 @@ import java.time.LocalDateTime;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class FriendRequest {
+public class FriendRequest implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
