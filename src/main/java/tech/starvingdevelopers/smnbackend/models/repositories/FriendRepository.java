664e5cdcade7a02344ea751abf65d58bf03ff888
Felipe <feliperos.simao@gmail.com>
2024-06-25 13:50:33 -0300

    feat(friends): initial implementation of friend system.

---
+++ src/main/java/tech/starvingdevelopers/smnbackend/models/repositories/FriendRepository.java
@@ -0,0 +1,17 @@
+package tech.starvingdevelopers.smnbackend.models.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.stereotype.Repository;
+import tech.starvingdevelopers.smnbackend.models.entities.Friend;
+
+import java.util.List;
+
+@Repository
+@EnableRedisRepositories
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+
+    @Query("SELECT f FROM Friend f WHERE f.leftUsername.username = :username OR f.rightUsername.username = :username")
+    List<Friend> findFriendsByUsername(String username);
+}
