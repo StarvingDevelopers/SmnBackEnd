e88a6844190a0cac44ad70c54bdc79a095b94393
Osnildo <juniorosnildo777@gmail.com>
2024-06-25 17:53:40 -0300

    feat(docs): documentation of the controller methods.

--- src/main/java/tech/starvingdevelopers/smnbackend/controllers/FriendController.java
+++ src/main/java/tech/starvingdevelopers/smnbackend/controllers/FriendController.java
@@ -17,12 +17,34 @@ public class FriendController {
         this.friendService = friendService;
     }
 
+    /**
+     * Rota de Recuperação de Lista de Amigos.
+     * <p>
+     *     Este método está mapeando para a solicitação HTTP GET no endpoint "/friend-list/{username}".
+     *     Busca informações da lista de amigos de um usuário. Delegando a tarefa para
+     *     o {@link FriendService}. Após a recuperação bem sucedida, retorna as informações
+     *     da lista em um objeto {@link GetFriendListRequestDTO} dentro de um {@link ResponseEntity} com status 200 OK
+     * </p>
+     * @param username nome do usuario para a recuperação da lista
+     * @return {@link ResponseEntity} contendo {@link GetFriendListRequestDTO} recuperado e status 200 OK
+     */
     @GetMapping("/friend-list/{username}")
     public ResponseEntity<GetFriendListRequestDTO> getFriendListRequests(@PathVariable String username) {
         List<Friend> friends = this.friendService.getFriendList(username);
         return ResponseEntity.ok(new GetFriendListRequestDTO(friends));
     }
 
+    /**
+     * Roda de Exclusão de Amigo.
+     * <p>
+     *     Esse método mapeado para a soliciatação HTTP DELETE no endpoint "/delete/{id}".
+     *     Recebe o id do usuário como uma variável de caminho delega a exclusão do id da lista
+     *     de amigos para o {@link FriendService}. Após a exclusão bem-sucedia do id, retorna uma resposta
+     *     sem conteúdo e status 200 OK
+     * </p>
+     * @param id código para a exclusão da amizade
+     * @return {@link ResponseEntity} sem conteúdo com status 200 OK
+     */
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteFriend(@PathVariable Long id) {
         this.friendService.deleteFriend(id);
